66f8e60611a39bbcb4d8dacb788445e19cfbf8d1
Ayokunle Paul <ayokunle@ulesson.com>
2020-08-26 20:26:36 +0100

    Enabled go module

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/AyokunlePaul/zagadat-api
+
+go 1.15
+
+require (
+	github.com/gin-contrib/zap v0.0.1 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	go.uber.org/zap v1.15.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
 
+func main() {
+	zapLogger, _ := zap.NewDevelopment()
+	router := gin.New()
+	router.Use(ginzap.Ginzap(zapLogger, time.RFC3339, true))
+	router.Use(ginzap.RecoveryWithZap(zapLogger, true))
+
+	router.GET("/zadadis", func(context *gin.Context) {
+		context.String(http.StatusOK, "Zagadat")
+	})
+
+	zapLogger.Fatal(router.Run(":8080").Error())
 }
